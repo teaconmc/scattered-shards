7187ddd0786dfe32c23b934befd18095512e0968
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2024-07-16 19:32:18 +0200

    Add progress tracking

--- src/main/java/net/modfest/scatteredshards/api/ScatteredShardsAPI.java
+++ src/main/java/net/modfest/scatteredshards/api/ScatteredShardsAPI.java
@@ -20,36 +20,38 @@ import net.modfest.scatteredshards.api.impl.ShardLibraryPersistentState;
 import net.modfest.scatteredshards.networking.S2CUpdateShard;
 
 public class ScatteredShardsAPI {
-	
+
 	public static final String MODIFY_SHARD_PERMISSION = ScatteredShards.permission("modify_shard");
-	
+
 	private static ShardLibraryPersistentState libraryPersistentState;
 	private static ShardCollectionPersistentState collectionPersistentState;
 	private static final ShardLibrary serverShardLibrary = new ShardLibraryImpl();
 	private static Map<UUID, ShardCollection> serverCollections = new HashMap<>();
 	public static ShardLibrary clientShardLibrary = null;
 	public static ShardCollection clientShardCollection = null;
+	public static ShardProgress clientShardProgress = null;
 	private static Thread serverThread = null;
 	private static Thread clientThread = null;
-	
-	
+	public static ShardProgress serverShardProgress = null;
+
+
 	public static ShardLibrary getServerLibrary() {
 		if (serverThread != null && !Thread.currentThread().equals(serverThread)) {
-			throw new IllegalStateException("getServerLibrary called from thread '"+Thread.currentThread().getName()+"'. This method can only be accessed from the server thread.");
+			throw new IllegalStateException("getServerLibrary called from thread '" + Thread.currentThread().getName() + "'. This method can only be accessed from the server thread.");
 		}
-		
+
 		return serverShardLibrary;
 	}
-	
+
 	@Environment(EnvType.CLIENT)
 	public static ShardLibrary getClientLibrary() {
 		if (clientThread != null && !Thread.currentThread().equals(clientThread)) {
-			throw new IllegalStateException("getClientLibrary called from thread '"+Thread.currentThread().getName()+"'. This method can only be accessed from the client thread.");
+			throw new IllegalStateException("getClientLibrary called from thread '" + Thread.currentThread().getName() + "'. This method can only be accessed from the client thread.");
 		}
-		
+
 		return clientShardLibrary;
 	}
-	
+
 	public static ShardCollection getServerCollection(UUID uuid) {
 		var collection = serverCollections.get(uuid);
 		if (collection == null) {
@@ -59,42 +61,72 @@ public class ScatteredShardsAPI {
 		}
 		return collection;
 	}
-	
+
+	public static void calculateShardProgress() {
+		var shardCountMap = new HashMap<Identifier, Integer>();
+		var totalCount = serverCollections.size();
+
+		serverCollections.forEach(((uuid, identifiers) -> {
+			identifiers.forEach(identifier -> {
+				shardCountMap.compute(identifier, (k, count) -> count != null ? 1 + count : 1);
+			});
+		}));
+
+		serverShardProgress = new ShardProgress(totalCount, shardCountMap);
+	}
+
 	public static ShardCollection getServerCollection(PlayerEntity player) {
 		return getServerCollection(player.getUuid());
 	}
-	
+
 	@ApiStatus.Internal
 	public static Map<UUID, ShardCollection> exportServerCollections() {
 		return serverCollections;
 	}
-	
+
 	@Environment(EnvType.CLIENT)
 	public static ShardCollection getClientCollection() {
 		if (clientThread != null && !Thread.currentThread().equals(clientThread)) {
-			throw new IllegalStateException("getClientCollection called from thread '"+Thread.currentThread().getName()+"'. This method can only be accessed from the client thread.");
+			throw new IllegalStateException("getClientCollection called from thread '" + Thread.currentThread().getName() + "'. This method can only be accessed from the client thread.");
 		}
-		
+
 		return clientShardCollection;
 	}
-	
-	
+
+
 	public static boolean triggerShardCollection(ServerPlayerEntity player, Identifier shardId) {
 		var collection = getServerCollection(player);
 		if (collection.add(shardId)) {
 			if (player.getServer() != null) collectionPersistentState.markDirty();
+
+			serverShardProgress.update(shardId, 1, serverCollections.size());
 			ServerPlayNetworking.send(player, new S2CUpdateShard(shardId, S2CUpdateShard.Mode.COLLECT));
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
+	public static boolean triggerShardUncollection(ServerPlayerEntity player, Identifier shardId) {
+		var collection = getServerCollection(player);
+		if (collection.remove(shardId)) {
+			if (player.getServer() != null) collectionPersistentState.markDirty();
+
+
+			serverShardProgress.update(shardId, -1, serverCollections.size());
+			ServerPlayNetworking.send(player, new S2CUpdateShard(shardId, S2CUpdateShard.Mode.UNCOLLECT));
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/* TODO: This does not get called currently */
 	@ApiStatus.Internal
 	public static void init() {
 		serverThread = Thread.currentThread();
 	}
-	
+
 	@ApiStatus.Internal
 	@Environment(EnvType.CLIENT)
 	public static void initClient() {
@@ -106,7 +138,7 @@ public class ScatteredShardsAPI {
 	public static void register(ShardCollectionPersistentState persistentState) {
 		ScatteredShardsAPI.collectionPersistentState = persistentState;
 	}
-	
+
 	public static void register(ShardLibraryPersistentState persistentState) {
 		ScatteredShardsAPI.libraryPersistentState = persistentState;
 	}
--- src/main/java/net/modfest/scatteredshards/client/screen/ShardTabletGuiDescription.java
+++ src/main/java/net/modfest/scatteredshards/client/screen/ShardTabletGuiDescription.java
@@ -1,6 +1,7 @@
 package net.modfest.scatteredshards.client.screen;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import io.github.cottonmc.cotton.gui.client.BackgroundPainter;
@@ -12,6 +13,7 @@ import io.github.cottonmc.cotton.gui.widget.WPlainPanel;
 import io.github.cottonmc.cotton.gui.widget.WScrollBar;
 import io.github.cottonmc.cotton.gui.widget.data.Axis;
 import io.github.cottonmc.cotton.gui.widget.data.Insets;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.util.Identifier;
 import net.modfest.scatteredshards.api.ShardCollection;
 import net.modfest.scatteredshards.api.ShardLibrary;
@@ -20,60 +22,61 @@ import net.modfest.scatteredshards.client.ScatteredShardsClient;
 import net.modfest.scatteredshards.client.screen.widget.WLeftRightPanel;
 import net.modfest.scatteredshards.client.screen.widget.WShardPanel;
 import net.modfest.scatteredshards.client.screen.widget.WShardSetPanel;
+import net.modfest.scatteredshards.networking.C2SRequestProgress;
 
 public class ShardTabletGuiDescription extends LightweightGuiDescription {
 	public static final int ROWS_PER_SCREEN = 5;
-	
+
 	protected final ShardCollection collection;
 	protected final ShardLibrary library;
-	
+
 	WShardPanel shardPanel = new WShardPanel();
 	WPlainPanel selectorPanel = new WPlainPanel();
 	WScrollBar shardSelectorScrollBar = new WScrollBar(Axis.VERTICAL);
 	WListPanel<Identifier, WShardSetPanel> shardSelector;
-	
+
 	public ShardTabletGuiDescription(ShardCollection collection, ShardLibrary library) {
 		this.collection = collection;
 		this.library = library;
-		
+
 		shardPanel.setShard(Shard.MISSING_SHARD);
 		shardPanel.setHidden(true);
-		
-		List<Identifier> ids = new ArrayList<>();
-		ids.addAll(this.library.shardSets().keySet());
-		ids.sort((a, b) -> a.getNamespace().compareTo(b.getNamespace()));
-		
+
+		List<Identifier> ids = new ArrayList<>(this.library.shardSets().keySet());
+		ids.sort(Comparator.comparing(Identifier::getNamespace));
+
 		shardSelector = new WListPanel<Identifier, WShardSetPanel>(ids, WShardSetPanel::new, this::configurePanel);
 		selectorPanel.setInsets(Insets.ROOT_PANEL);
-		
+
 		WLeftRightPanel root = new WLeftRightPanel(selectorPanel, shardPanel);
 		selectorPanel.add(shardSelector, 0, 0, getLayoutWidth(selectorPanel), getLayoutHeight(selectorPanel));
-		
-		
+
+		ClientPlayNetworking.send(C2SRequestProgress.INSTANCE);
+
 		this.setRootPanel(root);
-		
+
 		root.validate(this);
 	}
-	
+
 	private int getLayoutWidth(WPanelWithInsets panel) {
 		return panel.getWidth() - panel.getInsets().left() - panel.getInsets().right();
 	}
-	
+
 	private int getLayoutHeight(WPanelWithInsets panel) {
 		return panel.getHeight() - panel.getInsets().top() - panel.getInsets().bottom();
 	}
-	
+
 	private void configurePanel(Identifier setId, WShardSetPanel panel) {
 		panel.setSize(shardSelector.getWidth() - shardSelector.getScrollBar().getWidth(), 20);
 		panel.setShardConsumer(shardPanel::setShard);
 		panel.setShardSet(setId, library, collection);
 	}
-	
+
 	@Override
 	public void addPainters() {
 		selectorPanel.setBackgroundPainter(BackgroundPainter.createColorful(ScatteredShardsClient.LEFT));
 	}
-	
+
 	public static class Screen extends CottonClientScreen {
 		public Screen(ShardCollection collection, ShardLibrary library) {
 			super(new ShardTabletGuiDescription(collection, library));
